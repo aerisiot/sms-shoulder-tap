f49ea6ae803f80c3053ad3975ad29c68a085d588
vikram <rawat.vikram79@gmail.com>
2017-03-20 00:59:35 +0530

    added sudo to commands

--- client/src/main/scripts/start.sh
+++ client/src/main/scripts/start.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 cd /home/pi/piClient/client
 
-sudo -i
-/etc/init.d/gammu-smsd stop
-sleep 10
-/etc/init.d/gammu-smsd stop
+sudo /etc/init.d/gammu-smsd stop
 
-java -classpath *:./lib/* com.aeris.piDay.client.Initiator --clientEndpoint $AWS_HOST -clientId sdk-java -certificateFile myPie.cert.pem -privateKeyFile myPie.private.key $PUBLISH_DURATION &
+sleep 5
+
+sudo java -classpath *:./lib/* com.aeris.piDay.client.Initiator --clientEndpoint $AWS_HOST -clientId sdk-java -certificateFile myPie.cert.pem -privateKeyFile myPie.private.key $PUBLISH_DURATION
 
 
-/etc/init.d/gammu-smsd stop
+sleep 2
 
-sleep 8
-
-/etc/init.d/gammu-smsd start
+sudo /etc/init.d/gammu-smsd start
